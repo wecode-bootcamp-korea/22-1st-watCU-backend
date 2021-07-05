6eae0f215f7ad222ee91ea7e04dae62cdc3322e7
Jay9393 <tlswo1313@gmail.com>
2021-07-05 17:13:36 +0900

    ADD: User models

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class User(models.Model):
+    email    = models.CharField(max_length=100)
+    name     =  models.CharField(max_length=20)
+    password = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'users'
