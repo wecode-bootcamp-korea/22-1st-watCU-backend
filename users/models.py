22dbdb848b7ad665c4ecb3ccf794543294c8c886
Jay9393 <tlswo1313@gmail.com>
2021-07-06 20:53:41 +0900

    fix modeling

--- users/models.py
+++ users/models.py
@@ -1,16 +1,16 @@
-from django.db import models
+from django.db  import models
+from users.core import TimeStampModel
 
-class User(models.Model):
+class User(TimeStampModel):
     gender_choice = (
         ('male', 'Male'),
         ('female', 'Female'),
     )
-    email      = models.CharField(max_length=100)
+    email      = models.EmailField()
     name       = models.CharField(max_length=20)
     password   = models.CharField(max_length=200)
     gender     = models.CharField(max_length=20, choices=gender_choice, null=True)
     birth      = models.DateField(null=True)
-    signup_at  = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'users'
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from users.views import SignUpView, LoginView
 
 urlpatterns = [
-    path('/signup', SignUpView.as_view()),
-    path('/login', LoginView.as_view()),
+
 ]
