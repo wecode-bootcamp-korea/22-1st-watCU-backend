2a59fec7c2ba3179a80902c536254e60d90545f9
Jay9393 <80879214+Jay9393@users.noreply.github.com>
2021-07-06 21:39:25 +0900

    Update core.py

--- users/core.py
+++ users/core.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class TimeStampModel(models.Model):
     created_at  = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    updated_at  = models.DateTimeField(auto_now = True)
     
     class Meta:
         abstract = True
