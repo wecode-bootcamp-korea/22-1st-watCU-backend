6499d0879739dd8a80bf40a637209e10d7bd5f5f
yerang <grondin0425@gmail.com>
2021-07-07 20:05:53 +0900

    add: products/views.py DetailView => get querystring, divide detail/related

--- products/models.py
+++ products/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=20)
+    name      = models.CharField(max_length=20)
+    image_url = models.CharField(max_length=200)
 
     class Meta:
         db_table = 'categories'
--- products/views.py
+++ products/views.py
@@ -37,6 +37,7 @@ class MainView(View):
                         'price'          : product.price,
                         'image_url'      : image_url,
                         'average_rating' : average_rating,
+                        'description'    : product.description,
                     }
                 )
 
@@ -51,28 +52,58 @@ class MainView(View):
 class DetailView(View):
     def get(self, request, product_id):
         try:
-            product   = Product.objects.get(id=product_id)
-            image_url = product.image_set.first().image_url
-                
-            if Rating.objects.filter(product=product).exists():        
-                     average = Rating.objects.filter(product=product).aggregate(average=Avg('rating'))
-                     average_rating = round(average['average'], 1)
-            else:
-                average_rating = "0.0"
+            page = request.GET.get('page')
 
-            result = {
-                'category_name'  : product.category.name,
-                'korean_name'    : product.korean_name,
-                'english_name'   : product.english_name,
-                'price'          : product.price,
-                'description'    : product.description,
-                'image_url'      : image_url,
-                'average_rating' : average_rating,
-            }
+            if page == 'detail':
+                product  = Product.objects.get(id=product_id)
+                category = product.category
+
+                image_url = [image.image_url for image in product.image_set.all()]
+           
+                if Rating.objects.filter(product=product).exists():        
+                        average = Rating.objects.filter(product=product).aggregate(average=Avg('rating'))
+                        average_rating = round(average['average'], 1)
+                else:
+                    average_rating = 0.0
+
+                result = {
+                    'category_name'      : category.name,
+                    'category_image_url' : category.image_url,
+                    'korean_name'        : product.korean_name,
+                    'english_name'       : product.english_name,
+                    'price'              : product.price,
+                    'description'        : product.description,
+                    'main_image_url'     : image_url[0],
+                    'sub_image_url'      : image_url[1:],
+                    'average_rating'     : average_rating,
+                    'description'        : product.description,
+                }
         
-            return JsonResponse({'result': result}, status=200)
+                return JsonResponse({'result': result}, status=200)
+            
+            if page == 'related':
+                results = []
+
+                category  = Product.objects.get(id=product_id).category
+                products  = Product.objects.filter(category=category).exclude(id=product_id)
+
+                for product in products:
+                    image_url = product.image_set.first().image_url
+
+                    results.append(
+                        {
+                            'korean_name' : product.korean_name,
+                            'english_name': product.english_name,
+                            'price'       : product.price,
+                            'image_url'   : image_url,
+                        }
+                    )
+                
+                return JsonResponse({'results': results}, status=200)
 
         except Product.DoesNotExist:
             return JsonResponse({'message': 'PRODUCT_DOES_NOT_EXISTS'}, status=400)       
+        except Category.DoesNotExist:
+            return JsonResponse({'message': 'CATEGORY_DOES_NOT_EXISTS'}, status=400)       
         except Exception as error:
             return JsonResponse({'message': error}, status=400)
