a531d28cfc17bd65cecfd149c16cf6e40cc3509e
Jay9393 <tlswo1313@gmail.com>
2021-07-08 17:33:07 +0900

    modify: RESTful URI

--- likes/models.py
+++ likes/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.db.models.deletion import CASCADE
+from django.db                 import models
 
 class Wish(models.Model):
     product     = models.ForeignKey('products.Product', on_delete=CASCADE)
@@ -16,7 +16,7 @@ class Done(models.Model):
         db_table = 'done'
 
 class Like(models.Model):
-    user = models.ForeignKey('users.User', on_delete=CASCADE)
+    user    = models.ForeignKey('users.User', on_delete=CASCADE)
     comment = models.ForeignKey('comments.Comment', on_delete=CASCADE) 
 
     class Meta:
