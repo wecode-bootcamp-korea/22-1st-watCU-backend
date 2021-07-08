499275b3d4e2493b24b332ecbab05a7fdcf6cf7a
Jay9393 <80879214+Jay9393@users.noreply.github.com>
2021-07-08 13:07:00 +0900

    Update urls.py

--- likes/urls.py
+++ likes/urls.py
@@ -5,5 +5,5 @@ from likes.views import WishView, DoneView, LikeView
 urlpatterns = [
     path('/wish/<int:product_id>', WishView.as_view()),
     path('/done/<int:product_id>', DoneView.as_view()),
-    path('/<int:product_id>', LikeView.as_view()),
+    path('/<int:comment_id>', LikeView.as_view()),
 ]
