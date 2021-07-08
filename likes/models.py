4501dae2764351cd2faa9b533192fd3280e34cec
Jay9393 <tlswo1313@gmail.com>
2021-07-08 16:12:04 +0900

    Fix: likes models - likes : wish, done, like 로 모델 세분화 -> wish: 먹고싶어요 / done: 먹어봤어요 / like: 댓글 좋아요

--- likes/models.py
+++ likes/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-class Like(models.Model):
-    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
-    user    = models.ForeignKey('users.User', on_delete=models.CASCADE)
+class Wish(models.Model):
+    product     = models.ForeignKey('products.Product', on_delete=CASCADE)
+    user        = models.ForeignKey('users.User', on_delete=CASCADE)
 
     class Meta:
+        db_table = 'wish'
+
+class Done(models.Model):
+    product     = models.ForeignKey('products.Product', on_delete=CASCADE)
+    user        = models.ForeignKey('users.User', on_delete=CASCADE)
+
+    class Meta:
+        db_table = 'done'
+
+class Like(models.Model):
+    user = models.ForeignKey('users.User', on_delete=CASCADE)
+    comment = models.ForeignKey('comments.Comment', on_delete=CASCADE) 
+
+    class Meta:
+        
         db_table = 'likes'
