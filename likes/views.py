830ebf50a040070083a4e57efd41ceaa9c53eeff
Jay9393 <tlswo1313@gmail.com>
2021-07-09 13:09:54 +0900

    Modify: LikeView

--- likes/views.py
+++ likes/views.py
@@ -1,107 +1,67 @@
 import json
 
 from django.db.models.expressions import Exists
-from products.models              import Category, Product
+from django.db.models.fields      import CharField
 
 from django.http                  import JsonResponse
 from django.views                 import View
 from django.db                    import DataError
+from django.db.models             import Q
 
 from products.models              import Category, Product
 from users.models                 import User
-from likes.models                 import Done, Wish, Like
+from likes.models                 import Status, Like
 from users.utils                  import ConfirmUser
 
-class WishView(View):
-    # 먹고싶어요
+class StatusView(View):
+    # 먹고싶어요 먹어봤어요 상태 생성
     @ConfirmUser
-    def get(self, request, product_id):
+    def post(self, request):
         try:
-            user        = request.user
-            filter_like = Wish.objects.filter(product_id=product_id).filter(user_id=user.id).exists()
-
-            if not filter_like:
-                Wish.objects.create(
-                    user_id     = user.id,
-                    product_id  = product_id
-                    )
-                
-                return JsonResponse({'results': 'Success'}, status=201)
+            data    = json.loads(request.body)
+            product = request.GET.get('product', '')
+            user    = request.user
+            status  = data['status']
             
-            else:
-                raise ValueError
-    
-        except Exception as error:
-            return JsonResponse({'message': error}, status=400)
+            Status.objects.create(
+                status     = status,
+                user       = user,
+                product_id = product
+            )
 
-    # 먹고싶어요 삭제
+            return JsonResponse({'results': 'Success'}, status=201)
+
+        except KeyError:
+            return JsonResponse({'message': 'KeyError'}, status=400)
+            
+    # 먹고싶어요 먹어봤어요 상태 삭제
     @ConfirmUser
-    def delete(self, request, product_id):
+    def delete(self, request):
         try:
-            user        = request.user
-            filter_like = Wish.objects.filter(product_id=product_id).filter(user_id=user.id)
+            status  = request.GET.get('status', '')
+            product = request.GET.get('product')
+            user    = request.user
 
-            if filter_like.exists:
-                filter_like.delete()
-                return JsonResponse({'results': 'Success'}, status=201)
+            Status.objects.filter(user_id=user.id).filter(product_id=product).get(status=status).delete()
+
+            return JsonResponse({'results': 'Success'}, status=201)
+
+        except KeyError:
+            return JsonResponse({'message': 'KeyError'}, status=400)
             
-            else:
-                raise ValueError
-    
-        except Exception as error:
-            return JsonResponse({'message': error}, status=400)
-            
-class DoneView(View):
-    # 먹어봤어요
-    @ConfirmUser
-    def get(self, request, product_id):
-        try:
-            user        = request.user
-            filter_like = Done.objects.filter(product_id=product_id).filter(user_id=user.id).exists()
-
-            if not filter_like:
-                Done.objects.create(
-                    user_id     = user.id,
-                    product_id  = product_id,
-                    )
-                
-                return JsonResponse({'results': 'Success'}, status=201)
-            
-            else:
-                raise ValueError
-    
-        except Exception as error:
-            return JsonResponse({'message': error}, status=400)
-
-    # 먹어봤어요 삭제
-    @ConfirmUser
-    def delete(self, request, product_id):
-        try:
-            user        = request.user
-            filter_like = Done.objects.filter(product_id=product_id).filter(user_id=user.id)
-
-            if filter_like.exists:
-                filter_like.delete()
-                return JsonResponse({'results': 'Success'}, status=201)
-            
-            else:
-                raise ValueError
-    
-        except Exception as error:
-            return JsonResponse({'message': error}, status=400)
-
 class LikeView(View):
-    #댓글 좋아요
+    # 댓글 좋아요 생성
     @ConfirmUser
-    def get(self, request, comment_id):
+    def get(self, request):
         try:
+            comment     = request.GET.get('comment', '')
             user        = request.user
-            filter_like = Done.objects.filter(comment_id=comment_id).filter(user_id=user.id).exists()
+            like_user   = Like.objects.filter(comment_id=comment).filter(user_id=user.id).exists()
 
-            if not filter_like:
-                Done.objects.create(
+            if not like_user:
+                Like.objects.create(
                     user_id     = user.id,
-                    comment_id  = comment_id,
+                    comment_id  = comment,
                     )
                 
                 return JsonResponse({'results': 'Success'}, status=201)
@@ -109,22 +69,19 @@ class LikeView(View):
             else:
                 raise ValueError
     
-        except Exception as error:
-            return JsonResponse({'message': error}, status=400)
+        except KeyError:
+            return JsonResponse({'message': 'KeyError'}, status=400)
 
     # 댓글 좋아요 삭제
     @ConfirmUser
-    def delete(self, request, comment_id):
+    def delete(self, request):
         try:
-            user        = request.user
-            filter_like = Done.objects.filter(comment_id=comment_id).filter(user_id=user.id)
+            comment = request.GET.get('comment','')
+            user    = request.user
+            like    = Like.objects.filter(comment_id=comment).get(user_id=user.id)
 
-            if filter_like.exists:
-                filter_like.delete()
-                return JsonResponse({'results': 'Success'}, status=201)
-            
-            else:
-                raise ValueError
+            like.delete()
+            return JsonResponse({'results': 'Success'}, status=201)
     
-        except Exception as error:
-            return JsonResponse({'message': error}, status=400)
+        except KeyError:
+            return JsonResponse({'message': 'KeyError'}, status=400)
