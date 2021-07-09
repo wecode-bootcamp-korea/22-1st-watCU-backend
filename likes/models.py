c8e060a0acfd0582be0b6681dabe2e6d8a5cff33
Jay9393 <tlswo1313@gmail.com>
2021-07-09 10:55:45 +0900

    Modify: Like models

--- likes/models.py
+++ likes/models.py
@@ -1,22 +1,22 @@
 from django.db                 import models
 from django.db.models.deletion import CASCADE
 
-class Wish(models.Model):
-    product     = models.ForeignKey('products.Product', on_delete=CASCADE)
-    user        = models.ForeignKey('users.User', on_delete=CASCADE)
+class Status(models.Model):
+    
+    status_choice = (
+        ('wish', 'Wish'),
+        ('done', 'Done'),
+    )
+
+    status  = models.CharField(max_length=10, choices=status_choice)
+    user    = models.ForeignKey('users.User', on_delete=CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=CASCADE) 
 
     class Meta:
-        db_table = 'wish'
-
-class Done(models.Model):
-    product     = models.ForeignKey('products.Product', on_delete=CASCADE)
-    user        = models.ForeignKey('users.User', on_delete=CASCADE)
-
-    class Meta:
-        db_table = 'done'
+        db_table = 'statuses' 
 
 class Like(models.Model):
-    user    = models.ForeignKey('users.User', on_delete=CASCADE)
+    user = models.ForeignKey('users.User', on_delete=CASCADE)
     comment = models.ForeignKey('comments.Comment', on_delete=CASCADE) 
 
     class Meta:
