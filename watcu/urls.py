0e4e2b063571b2d1f5b1d02947a71a4354dbd9af
yerang <grondin0425@gmail.com>
2021-07-12 17:03:50 +0900

    merge origin main

--- watcu/urls.py
+++ watcu/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path('ratings', include('ratings.urls')),
     path('users', include('users.urls')),
     path('products', include('products.urls')),
-    path('comments', include('comments.ur')),
+    # path('comments', include('comments.urls')),
 ]
