a01f9d1a4ab94a9cc93c55191e71ca2ae6e2a0ad
yerang <grondin0425@gmail.com>
2021-07-13 12:29:29 +0900

    db_uploade complete 1

--- db_uploader.py
+++ db_uploader.py
@@ -5,26 +5,28 @@ django.setup()
 
 from products.models import Product, Image
 
-# CSV_PATH_PRODUCTS = 'products.csv'
+CSV_PATH_PRODUCTS = 'products.csv'
 
-# with open(CSV_PATH_PRODUCTS) as in_file:
-#     data_reader = csv.reader(in_file)
-#     next(data_reader, None)
+with open(CSV_PATH_PRODUCTS) as in_file:
+    data_reader = csv.reader(in_file)
+    next(data_reader, None)
 
-#     for row in data_reader:
-#         korean_name = row[1]
-#         english_name = row[2]
-#         price = row[3]
-#         description = row[4]
-#         category_id = row[5]
+    for row in data_reader:
+        id = row[0]
+        korean_name = row[1]
+        english_name = row[2]
+        price = row[3]
+        description = row[4]
+        category_id = row[5]
 
-#         Product.objects.create(
-#             korean_name = korean_name,
-#             english_name = english_name,
-#             price = price,
-#             description = description,
-#             category_id = category_id,
-#         )
+        Product.objects.create(
+            id = id,
+            korean_name = korean_name,
+            english_name = english_name,
+            price = price,
+            description = description,
+            category_id = category_id,
+        )
 
 CSV_PATH_PRODUCTS = 'images.csv'
 
@@ -33,10 +35,12 @@ with open(CSV_PATH_PRODUCTS) as in_file:
     next(data_reader, None)
 
     for row in data_reader:
+        id = row[0]
         image_url = row[1]
         product_id = row[2]
 
         Image.objects.create(
+            id = id,
             image_url = image_url,
             product_id = product_id,
         )
