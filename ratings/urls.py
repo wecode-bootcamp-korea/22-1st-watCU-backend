f31501ab6900a07798b1c967051de668c6e21c39
yerang <grondin0425@gmail.com>
2021-07-14 14:05:11 +0900

    add: RatingCountView => count all ratings

--- ratings/urls.py
+++ ratings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from ratings.views import RatingView, RatingGraphView
+from ratings.views import RatingView, RatingGraphView, RatingCountView
 
 urlpatterns =[
+    path('', RatingCountView.as_view()),
     path('/products/<int:product_id>', RatingView.as_view()),
     path('/products/<int:product_id>/graph', RatingGraphView.as_view()),
 ]
